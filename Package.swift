055b96f32108792e204d834dda6e9d325ab9a00e
johnrnyquist <john@nyquist.net>
2025-02-23 15:20:19 -0700

    Updaated minum iOS to 12.0 for Swift 5.9 for both target and test target

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "ActionSwift3",
-    products: [
-        .library(name: "ActionSwift3", targets: ["ActionSwift3"])
-    ],
+    platforms: [.iOS(.v12)],  // iOS 12.0
+    products: [.library(name: "ActionSwift3", targets: ["ActionSwift3"])],
     targets: [
-        .target(name: "ActionSwift3", path: "Sources/ActionSwift3", deploymentTargets: [.iOS("8.0")]),
-        .testTarget(name: "ActionSwift3Tests", dependencies: ["ActionSwift3"], path: "Tests/ActionSwift3Tests", deploymentTargets: [.iOS("15.0")])
+        .target(name: "ActionSwift3", path: "Sources/ActionSwift3"),
+        .testTarget(name: "ActionSwift3Tests", dependencies: ["ActionSwift3"], path: "Tests/ActionSwift3Tests")
     ]
 )
